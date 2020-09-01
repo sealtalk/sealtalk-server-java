0d0b4c31caf2206b1d7b0413a38d7818e089194c
yujianlu <yujianlu@rongcloud.cn>
2020-09-01 12:14:11 +0800

    跨域处理

--- src/main/java/com/rcloud/server/sealtalk/interceptor/RequestInterceptor.java
+++ src/main/java/com/rcloud/server/sealtalk/interceptor/RequestInterceptor.java
@@ -78,6 +78,14 @@ public class RequestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Headers","*");
+        if(request.getMethod().equals("OPTIONS")){
+            response.setStatus(HttpServletResponse.SC_OK);
+            return false;
+        }
+
         ServerApiParams serverApiParams = new ServerApiParams();
         serverApiParams.setTraceId(UUID.randomUUID().toString());
         String uri = request.getRequestURI();
