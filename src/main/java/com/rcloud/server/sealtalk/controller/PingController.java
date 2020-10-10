18f1838614eec4119df1a4b05b26b35b832c9458
yujianlu <yujianlu@rongcloud.cn>
2020-10-10 13:49:15 +0800

    新增优雅停机配置

--- src/main/java/com/rcloud/server/sealtalk/controller/PingController.java
+++ src/main/java/com/rcloud/server/sealtalk/controller/PingController.java
@@ -3,6 +3,7 @@ package com.rcloud.server.sealtalk.controller;
 import com.rcloud.server.sealtalk.model.response.APIResult;
 import com.rcloud.server.sealtalk.model.response.APIResultWrap;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,18 @@ public class PingController {
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public APIResult<Object> ping() {
         return APIResultWrap.ok();
+    }
+
+    /**
+     * 测试用
+     * @return
+     * @throws InterruptedException
+     */
+    @GetMapping("/testShutdown")
+    public String demo() throws InterruptedException {
+        // 模拟业务耗时处理流程
+        Thread.sleep(20 * 1000L);
+        return "hello";
+
     }
 }
