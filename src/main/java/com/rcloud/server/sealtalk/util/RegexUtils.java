7fd130cb5fc85066a10a6a6cd19d4d79702fa5b5
yujianlu <yujianlu@rongcloud.cn>
2020-10-29 16:25:54 +0800

    bugfix:支持166号段手机号

--- src/main/java/com/rcloud/server/sealtalk/util/RegexUtils.java
+++ src/main/java/com/rcloud/server/sealtalk/util/RegexUtils.java
@@ -39,7 +39,7 @@ public class RegexUtils {
      * @return 验证成功返回true，验证失败返回false
      */
     public static boolean checkMobile(String mobile) {
-        String regex = "(\\+\\d+)?1[345789]\\d{9}$";
+        String regex = "(\\+\\d+)?1[3456789]\\d{9}$";
         return Pattern.matches(regex,mobile);
     }
 
