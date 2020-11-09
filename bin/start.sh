f96915983cbefb63484f379772c6d078484ed830
yujianlu <yujianlu@rongcloud.cn>
2020-11-09 19:03:26 +0800

    修改脚本

--- bin/start.sh
+++ bin/start.sh
@@ -5,5 +5,5 @@ if [ $? -eq 0 ];then
   echo ‘can not start,sealtalk-server is running now,please shutdown first!‘
 else
     echo "startup server......"
-    nohup java -server -Xms1G -Xmx4G  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -jar sealtalk-server.jar --server.port=8080 --spring.profiles.active=prod  &
+    nohup java -server -Xms1G -Xmx4G  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./ -jar sealtalk-server.jar --server.port=8080 --spring.profiles.active=dev  &
 fi
